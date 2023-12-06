7890d5d8302d99f74f51afe37d0da0fabdab9132
alelvieira <84207873+alelvieira@users.noreply.github.com>
2023-12-05 22:26:11 -0300

    Add files via upload

---
+++ src/main/java/com/mapadavida/mdvBackend/MdvBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mapadavida.mdvBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdvBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdvBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mapadavida/mdvBackend/MdvBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapadavida.mdvBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdvBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
